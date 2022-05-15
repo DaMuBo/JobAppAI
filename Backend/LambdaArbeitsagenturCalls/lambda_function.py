99f55ecddb2de1df5c119170364d9c29c0d8def2
DaMuBo <daniel910527@googlemail.com>
2022-05-15 20:58:24 +0200

    nur data related jobs will be added

--- Backend/LambdaArbeitsagenturCalls/lambda_function.py
+++ Backend/LambdaArbeitsagenturCalls/lambda_function.py
@@ -1,27 +1,24 @@
 """
-Development part of the lambda function for the api calls 
+Development part of the lambda function for the api calls
 from the Arbeitsagentur API
 
 funktionen:
     get_jwt(): Holt das Access Token zur Authentifizierung bei der API
-    
+
     search(): Führt die Suche aus und gibt die Referenznr in einer Liste zurück mit welcher die Jobbeschreibungen gefunden werden können
-    
+
     job_details(): Holt die Jobdetail Informationen für einen konkreten Job aus der API
-    
+
     job_rotator(): kombiniert die Funktionen get_jwt, search und job_details. Außerdem werden die Ergebnisse in einer Liste gesammelt
         und zurückgegeben
-    
+
     lambda_handler:
         definiert rahmenbedingungen und führt den job_rotator() aus. Die Ergebnisse werden unter ihrer refnr im S3 Bucket gespeichert.
-
 """
 
-import json
-import boto3
-import requests
 import base64
-
+import requests
+import boto3
 
 def get_jwt():
     """fetch the jwt token object"""
@@ -46,21 +43,20 @@ def search(jwt, what, page, limit):
     """search for jobs. params can be found here: https://jobsuche.api.bund.dev/
     Inputs:
     --------------------------------------
-    
+
     jwt:
         Das Access Token für den Zugriff
-        
+
     what: 
         Der Suchbergriff nach dem gesucht wird
-    
+
     page:
         Die Seite die aufgesucht werden soll
-    
+
     limit:
         Wenn nicht None dann wird eine Info mit veroeffentlichseit an parameter angehängt zur Eingrenzung
-    
     """
-    
+
     params = (
         ('angebotsart', '1'),
         ('page', page),
@@ -82,7 +78,7 @@ def search(jwt, what, page, limit):
 
     response = requests.get('https://rest.arbeitsagentur.de/jobboerse/jobsuche-service/pc/v4/app/jobs',
                             headers=headers, params=params, verify=False)
-    
+
     return response.json()
 
 
@@ -108,84 +104,87 @@ def job_details(jwt, job_ref):
 def job_rotator(limit=None, what='data'):
     """
     Input:
-    
+
         limit: Integer/None
             wenn nicht None dann gibt es die maximale Anzahl an Tage in die Vergangenheit an.
     """
     checker = True
     page = 1
-    myList = []
-    
-    while(checker):
+    mylist = []
+
+    while(checker is True):
         jwt = get_jwt()
         result = search(jwt["access_token"], what, page, limit)
         if 'stellenangebote' in result.keys():
             for row in result['stellenangebote']:
                 output = job_details(jwt["access_token"], row["refnr"])
-                myList.append(output)
+                mylist.append(output)
             page += 1
         else:
             checker=False
     
-    return myList
-        
+    return mylist
+
 
 def dict_to_item(raw):
     """
     takes a dictionary and is returning the datatype of each item in a format for writing it into a dynamoDB
     is using recursive calls on itself to get the informations out of lists and dicitonarys
-    
+
     """
-    if type(raw) is dict:
+    if isinstance(raw,dict):
         resp = {}
         for k,v in raw.items():
-            if type(v) is str:
+            if isinstance(v,str):
                 resp[k] = {
                     'S': v
                 }
-            elif type(v) is int or type(v) is float:
+            elif isinstance(v,int) or isinstance(v,float):
                 resp[k] = {
                     'N': str(v)
                 }
-            elif type(v) is dict:
+            elif isinstance(v,dict):
                 resp[k] = {
                     'M': dict_to_item(v)
                 }
-            elif type(v) is list:
+            elif isinstance(v,list):
                 resp[k] = {"L":[]}
                 for i in v:
-                    if type(i) is dict:
+                    if isinstance(i,dict):
                         resp[k]["L"].append({"M":dict_to_item(i)})
                         print(resp[k])
                     else:
                         resp[k]["L"].append(dict_to_item(i))
                         print(resp[k])
-                    
         return resp
-    elif type(raw) is str:
+    elif isinstance(raw,str):
         return {
             'S': raw
         }
-    elif type(raw) is int or type(raw) is float:
+    elif isinstance(raw,int) or isinstance(raw,float):
         return {
             'N': str(raw)
         }
-
+    return {
+        'S':str(raw)
+    }
 
 def lambda_handler(event, context):
-    # TODO implement
-    dynamodb = boto3.client("dynamodb")   
-    
+    """
+    Finale Funktion die von Lambda aufgerufen wird und die Daten ausführt
+    """
+    dynamodb = boto3.client("dynamodb")
+
     limit = 0 # days since the writing for initial load = None After that = 0 or 1
     what = 'data' # what is searched for'
-    
+
     # the json file to write
-    myList = job_rotator(limit, what)
-    for row in myList:
+    mylist = job_rotator(limit, what)
+    for row in mylist:
         if 'refnr' in row.keys():
             if 'titel' in row.keys():
-                if "data" in row['titel'].lower() or "analyst" in row['titel'].lower():
-                    myItem = dict_to_item(row)
-                    dynamodb.put_item(TableName='JobData', Item=myItem)
+                if 'data' in row['titel'] or 'analyst' in row['titel']:
+                    myitem = dict_to_item(row)
+                    dynamodb.put_item(TableName='JobData', Item=myitem)
 
     print('Put Complete Writing Data to Bucket')
