a4c68d9b06377d4a3694d4a917539c1e36e293ca
DaMuBo <daniel910527@googlemail.com>
2022-05-22 21:59:50 +0200

    skill extractor final hinzugefÃ¼gt so wie er live geht

--- Backend/SkillExtractor/lambda_handler.py
+++ Backend/SkillExtractor/lambda_handler.py
@@ -13,25 +13,85 @@ extractor_rule:
 """
 import os
 import json
+import re
+from datetime import datetime
+from datetime import timedelta
 import boto3
+from boto3.dynamodb.conditions import Key
 import spacy
 
-def get_data():
+
+def clean_text(text):
     """
-    Hole daten aus der dynamo DB nach dem beschriebenen muster und gib diese zurück
+    Bereinigung von möglichen Sonderzeichen
     """
-    
-    
-def write_data():
+    cleaned = re.sub(r"[\(\[].*?[\)\]]", "", text)
+    return cleaned
+
+def get_min_datum(timeframe=0):
     """
-    Aktualisiere die Daten mit den angereicherten Skills in der dynamoDB
+    Ermittelt das minum datum ausgehend von heute ()
     """
-    
-def calculate_skills():
+    datum = datetime.now()
+    # ziehe die tage ab
+    return datum - timedelta(days=timeframe)
+
+
+def dict_to_item(raw):
     """
-    Extrahiere die skills aus den Texten und gib diese für jeden fall entsprechend zurück
-    Wenn keine skills gefunden wurden gib eine leere liste zurück
+    takes a dictionary and is returning the datatype of each item in a format for writing it into a dynamoDB
+    is using recursive calls on itself to get the informations out of lists and dicitonarys
     """
+    if isinstance(raw,dict):
+        resp = {}
+        for k,v in raw.items():
+            if isinstance(v,str):
+                resp[k] = {
+                    'S': v
+                }
+
+            elif isinstance(v,bool):
+                resp[k] = {
+                    'S': str(v)
+                }
+            elif isinstance(v,(int,float)):
+                resp[k] = {
+                    'N': str(v)
+                }
+            elif isinstance(v,dict):
+                resp[k] = {
+                    'M': dict_to_item(v)
+                }
+            elif isinstance(v,list):
+                resp[k] = {"L":[]}
+                for i in v:
+                    if isinstance(i,dict):
+                        resp[k]["L"].append({"M":dict_to_item(i)})
+                    else:
+                        resp[k]["L"].append(dict_to_item(i))
+        return resp
+    elif isinstance(raw,str):
+        return {
+            'S': raw
+        }
+    elif isinstance(raw,(int,float)):
+        return {
+            'N': str(raw)
+        }
+    return {
+        'S':str(raw)
+    }
+
+def query_job_data(datum, dynamodb):
+    """
+    Lade die Daten aus der DB
+    """
+    table = dynamodb.Table('JobData')
+    response = table.query(
+        KeyConditionExpression=Key('aktuelleVeroeffentlichungsdatum').eq(datum),
+        #Limit = limit
+    )
+    return response['Items']
 
 def download_dir(client, resource, dist, local='/tmp', bucket='s3bucket'):
     """
@@ -43,7 +103,7 @@ def download_dir(client, resource, dist, local='/tmp', bucket='s3bucket'):
         if result.get('Contents') is not None:
             for file in result.get('Contents'):
                 if not os.path.exists(os.path.dirname(local + os.sep + file.get('Key'))):
-                    os.makedirs(os.path.dirname(local + os.sep + file.get('Key') + os.sep ))
+                    os.makedirs(os.path.dirname(local + os.sep + file.get('Key')))
                 if file.get('Key') != dist + '/' :
                     if file.get('Size') != 0: # wenn das file size = 0 dann ist es ein directory und soll ignoriert werden
                         resource.meta.client.download_file(bucket, file.get('Key'), local + '/' + file.get('Key'))
@@ -55,20 +115,30 @@ def lambda_handler(event, context):
     Main Function zum ausführen der restlichen Codes und laden der benötigten Daten aus den Datenbanken
     Schreibt auch die Ergebnisse zurück in die DB
     """
-
     client = boto3.client('s3')
     resource = boto3.resource('s3')
+    dynamodb = boto3.resource('dynamodb')
+    dynclient = boto3.client('dynamodb')
     bucket_name =  "job-app-data-bucket"
     object_key = 'models/skill_ner_model'  # replace object key
     if (os.path.isdir("/tmp/" + object_key)==False):
         download_dir(client, resource, object_key, '/tmp',bucket_name)
-        print(os.path.isdir("/tmp/" + object_key))
     
-    #nlp = spacy.load('/tmp/' + object_key)
-    # hier text reinladen
-    #text = "Hallo ich bin ein test text. Bittee sag mir ob ich ok bin und ob ich python oder R kann."
-    #oc = nlp(text)
-    #print(oc)
+    datobj = get_min_datum(1)
+    nlp = spacy.load('/tmp/' + object_key)
+    while datobj <= datetime.now():
+        datum =  str(datobj.year) + '-' + str(datobj.month).zfill(2) + '-' + str(datobj.day).zfill(2)
+        datobj = datobj + timedelta(days=1)
+        response = query_job_data(datum,dynamodb)
+        for row in response:
+            if 'stellenbeschreibung' in row.keys():
+                doc = nlp(clean_text(row['stellenbeschreibung']))
+                if len(doc.ents) > 0:
+                    row['skills'] = list(set([x.text for x in doc.ents]))
+                    print(datum, row['skills'])
+                    myitem = dict_to_item(row)
+                    dynclient.put_item(TableName='JobData', Item=myitem)
+
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
