c14db962ac77fb409e6f042da171591b94345d62
sebischm <sebi.schmor@gmail.com>
2022-05-22 19:44:32 +0200

    Paar mehr visualisiert

--- machine_learning/05_visu_found_entities.py
+++ machine_learning/05_visu_found_entities.py
@@ -8,9 +8,14 @@ import os
 PROJECT_ROOT_DIR = "."
 visu_path = os.path.join(PROJECT_ROOT_DIR, "visu")
 models_path = os.path.join(PROJECT_ROOT_DIR, "models")
+data_path = os.path.join(PROJECT_ROOT_DIR, "data")
 os.makedirs(visu_path, exist_ok=True)
 
 skill_ner_model_path = os.path.join(models_path, "skill_ner_model")
+
+json_folder = "labeling_job_1_output"
+json_folder_path = os.path.join(data_path, json_folder)
+dateien = os.listdir(json_folder_path)[:4]
 
 
 def load_data(file):
@@ -19,23 +24,25 @@ def load_data(file):
     return data
 
 
-file_content = load_data(
-    r"data\labeling_job_1_output\labeling_job_1_output_0.json"
-)
-test = file_content.get("source")
+for file in dateien:
 
+    filepath = os.path.join(json_folder_path, file)
 
-def clean_text(text):
-    cleaned = re.sub(r"[\(\[].*?[\)\]]", "", text)
-    return cleaned
+    file_content = load_data(filepath)
+    test = file_content.get("source")
 
+    def clean_text(text):
+        cleaned = re.sub(r"[\(\[].*?[\)\]]", "", text)
+        return cleaned
 
-test = clean_text(test)
+    test = clean_text(test)
 
-nlp = spacy.load(skill_ner_model_path)
-doc = nlp(test)
+    nlp = spacy.load(skill_ner_model_path)
+    doc = nlp(test)
 
-html = displacy.render(doc, style="ent")
-html_path = os.path.join(visu_path, "found_entities.html")
-with open(html_path, "w", encoding="utf-8") as f:
-    f.write(html)
+    html = displacy.render(doc, style="ent")
+    html_path = os.path.join(
+        visu_path, file.replace(".json", "") + "_found_entities.html"
+    )
+    with open(html_path, "w", encoding="utf-8") as f:
+        f.write(html)
