8b049e5e666211ab70aeac5e75db395fb1780951
DaMuBo <daniel910527@googlemail.com>
2022-05-28 12:11:55 +0200

    config file entfernt mit den secrets

--- Backend/GetSkills/lambda_function.py
+++ Backend/GetSkills/lambda_function.py
@@ -8,36 +8,33 @@ from datetime import datetime
 import boto3
 from boto3.dynamodb.conditions import Key
 
+def get_min_datum(timeframe=0):
+    """
+    Ermittelt das minum datum ausgehend von heute ()
+    """
+    datum = datetime.now()
+    # ziehe die tage ab
+    return datum - timedelta(days=timeframe)
+
+
 def query_job_data(datum, dynamodb):
     """
-    querying job data based on partition key
+    Lade die Daten aus der DB
     """
     table = dynamodb.Table('JobData')
     response = table.query(
         KeyConditionExpression=Key('aktuelleVeroeffentlichungsdatum').eq(datum),
+        #Limit = limit
     )
     return response['Items']
 
 def lambda_handler(event, context):
     """
-    Function um von dynamo DB die benötigten Datensets zu laden und eine Manifest Datei zu erzeugen
+    Function um von dynamo DB die benötigten Datensets zu laden und in json format an die API zurück zu liefern.
     """
     dynamodb = boto3.resource('dynamodb')
-    s3 = boto3.resource("s3")
-    datobj = datetime.now()
-    datum =  str(datobj.year) + '-' + str(datobj.month).zfill(2) + '-' + str(datobj.day).zfill(2)
-
-    bucket_name = 'job-app-data-bucket'
-    filename = "mymanifest.jsonl"
-
-    response = query_job_data(datum, dynamodb)
-    s3_path = 'manifest/' +filename
-    with open('/tmp/output.jsonl', 'w') as outfile:
-        for row in response:
-            if 'stellenbeschreibung' in row.keys():
-                mydict = {'source':row['stellenbeschreibung']}
-                json.dump(mydict, outfile)
-                outfile.write('\n')
-
-    s3.meta.client.upload_file('/tmp/output.jsonl', bucket_name, s3_path)
-    print('Created Manifest Successfull')
+    dynclient = boto3.client('dynamodb')
+    
+    return {
+        
+    }
